69ce8a2642a84f696070b525fb8e3fdd099bc022
Quang Hoang <126427971+theantigone@users.noreply.github.com>
2025-04-11 00:30:26 -0400

    Upload ``5-2``

---
+++ 5-2/exercise.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include "limits.h"
+#include "../munit/munit.h"
+
+typedef union {
+  int value;
+  unsigned int err;
+} val_or_err_t;
+
+int main() {
+  val_or_err_t lanes_score = {
+    .value = -420
+  };
+  printf("value (set): %d\n", lanes_score.value);
+  printf("err (unset): %u\n", lanes_score.err);
+
+  val_or_err_t teejs_score = {
+    .err = UINT_MAX
+  };
+  printf("value (unset): %d\n", teejs_score.value);
+  printf("err (set): %u\n", teejs_score.err);
+}
+
