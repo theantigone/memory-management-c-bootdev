b98694f2069891b4ec1a7fa7842e2402da1a3336
Quang Hoang <126427971+theantigone@users.noreply.github.com>
2026-02-03 22:46:21 -0500

    Upload `8-4`

---
+++ 8-4/snekstack.c
@@ -0,0 +1,56 @@
+#include "snekstack.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+void stack_free(stack_t *stack) {
+  // ?
+	if(!stack)
+		return;
+	free(stack->data);
+	free(stack);
+}
+
+// don't touch below this line
+
+void *stack_pop(stack_t *stack) {
+  if (stack->count == 0) {
+    return NULL;
+  }
+
+  stack->count--;
+  return stack->data[stack->count];
+}
+
+void stack_push(stack_t *stack, void *obj) {
+  if (stack->count == stack->capacity) {
+    stack->capacity *= 2;
+    void **temp = realloc(stack->data, stack->capacity * sizeof(void *));
+    if (temp == NULL) {
+      stack->capacity /= 2;
+      exit(1);
+    }
+    stack->data = temp;
+  }
+  stack->data[stack->count] = obj;
+  stack->count++;
+  return;
+}
+
+stack_t *stack_new(size_t capacity) {
+  stack_t *stack = malloc(sizeof(stack_t));
+  if (stack == NULL) {
+    return NULL;
+  }
+
+  stack->count = 0;
+  stack->capacity = capacity;
+  stack->data = malloc(stack->capacity * sizeof(void *));
+  if (stack->data == NULL) {
+    free(stack);
+    return NULL;
+  }
+
+  return stack;
+}
+
