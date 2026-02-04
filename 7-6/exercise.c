db45033127594b631feec11b19741eb18a923db6
Quang Hoang <126427971+theantigone@users.noreply.github.com>
2026-02-03 19:14:40 -0500

    Upload `7-6`

--- 7-6/exercise.c
+++ 7-6/exercise.c
@@ -3,10 +3,12 @@
 
 void swap(void *vp1, void *vp2, size_t size) {
   // ?
-	int *buffer=malloc(size*sizeof(size));
+	void*buffer=malloc(size);
 	if(!buffer)
 		return;
+	memcpy(buffer,vp1,size);
 	memcpy(vp1,vp2,size);
+	memcpy(vp2,buffer,size);
 	free(buffer);
 }
 
