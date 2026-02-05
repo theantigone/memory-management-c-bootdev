777cb749832641ce5266dea05ed850486e5fa8d6
Quang Hoang <126427971+theantigone@users.noreply.github.com>
2026-02-05 18:15:59 -0500

    idk

---
+++ 9-6/snekobject.c
@@ -0,0 +1,82 @@
+#include "snekobject.h"
+#include <stdlib.h>
+#include <string.h>
+
+snek_object_t *new_snek_array(size_t size) {
+  // ?
+	snek_object_t*ptr=malloc(sizeof(snek_object_t));
+	if(!ptr)
+		return NULL;
+	size_t len=sizeof(snek_object_t)/sizeof(snek_object_t[0]);
+	snek_object_t**arr=calloc(len,sizeof(snek_object_t*));
+	if(!arr){
+		free(arr);
+		return NULL;
+	}
+	ptr->kind=ARRAY;
+	ptr->data.v_array.size=size;
+	ptr->data.v_array.elements=arr;
+	return ptr;
+}
+
+// don't touch below this line
+
+snek_object_t *new_snek_vector3(snek_object_t *x, snek_object_t *y,
+                                snek_object_t *z) {
+  if (x == NULL || y == NULL || z == NULL) {
+    return NULL;
+  }
+
+  snek_object_t *obj = malloc(sizeof(snek_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  obj->kind = VECTOR3;
+  obj->data.v_vector3 = (snek_vector_t){.x = x, .y = y, .z = z};
+
+  return obj;
+}
+
+snek_object_t *new_snek_integer(int value) {
+  snek_object_t *obj = malloc(sizeof(snek_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  obj->kind = INTEGER;
+  obj->data.v_int = value;
+  return obj;
+}
+
+snek_object_t *new_snek_float(float value) {
+  snek_object_t *obj = malloc(sizeof(snek_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  obj->kind = FLOAT;
+  obj->data.v_float = value;
+  return obj;
+}
+
+snek_object_t *new_snek_string(char *value) {
+  snek_object_t *obj = malloc(sizeof(snek_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  int len = strlen(value);
+  char *dst = malloc(len + 1);
+  if (dst == NULL) {
+    free(obj);
+    return NULL;
+  }
+
+  strcpy(dst, value);
+
+  obj->kind = STRING;
+  obj->data.v_string = dst;
+  return obj;
+}
+
