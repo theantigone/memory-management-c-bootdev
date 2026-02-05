9f15f827d80815c75ce577538621ceb0d72367d2
Quang Hoang <126427971+theantigone@users.noreply.github.com>
2026-02-05 17:47:00 -0500

    Update workings

---
+++ 9-5/snekobject.c
@@ -0,0 +1,63 @@
+#include "snekobject.h"
+#include <stdlib.h>
+#include <string.h>
+
+snek_object_t *new_snek_vector3(snek_object_t *x, snek_object_t *y,
+                                snek_object_t *z) {
+  // ?
+	if(!x||!y||!z)
+		return NULL;
+	snek_object_t*obj=malloc(sizeof(snek_object_t));
+	if(!obj)
+		return NULL;
+	obj->kind=VECTOR3;
+	obj->data.v_vector3.x=x;
+	obj->data.v_vector3.y=y;
+	obj->data.v_vector3.z=z;
+	return obj;
+}
+
+// don't touch below this line
+
+snek_object_t *new_snek_integer(int value) {
+  snek_object_t *obj = malloc(sizeof(snek_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  obj->kind = INTEGER;
+  obj->data.v_int = value;
+  return obj;
+}
+
+snek_object_t *new_snek_float(float value) {
+  snek_object_t *obj = malloc(sizeof(snek_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  obj->kind = FLOAT;
+  obj->data.v_float = value;
+  return obj;
+}
+
+snek_object_t *new_snek_string(char *value) {
+  snek_object_t *obj = malloc(sizeof(snek_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  int len = strlen(value);
+  char *dst = malloc(len + 1);
+  if (dst == NULL) {
+    free(obj);
+    return NULL;
+  }
+
+  strcpy(dst, value);
+
+  obj->kind = STRING;
+  obj->data.v_string = dst;
+  return obj;
+}
+
---
+++ 9-5/snekobject.h
@@ -0,0 +1,30 @@
+struct snek_object_t;
+typedef struct{
+    snek_object_t*  x;
+    snek_object_t*  y;
+    snek_object_t*  z;
+}snek_vector_t;
+typedef enum SnekObjectKind {
+  INTEGER,
+  FLOAT,
+  STRING,
+  VECTOR3,
+} snek_object_kind_t;
+
+typedef union SnekObjectData {
+  int v_int;
+  float v_float;
+  char *v_string;
+  snek_vector_t v_vector3;
+} snek_object_data_t;
+
+typedef struct SnekObject {
+  snek_object_kind_t kind;
+  snek_object_data_t data;
+} snek_object_t;
+
+snek_object_t *new_snek_integer(int value);
+snek_object_t *new_snek_float(float value);
+snek_object_t *new_snek_string(char *value);
+snek_object_t *new_snek_vector3(snek_object_t*x,snek_object_t*y,snek_object_t*z);
+
